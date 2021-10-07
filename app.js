0d7cb83b9aba19122c4fc4e170d25b70f6f559f0
Azel Opeña <azel.opena22@gmail.com>
2021-10-07 12:15:12 +0800

    First Commit

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-const reasonInput = document.querySelector('#input-reason');
-const amountInput = document.querySelector('#input-amount');
+const nameInput = document.querySelector('#input-name');
+const numberInput = document.querySelector('#input-number');
 const cancelButton = document.querySelector('#btn-clear');
 const addButton = document.querySelector('#btn-add');
-const expensesList = document.querySelector('#expenses-list');
-const totalExpensesOutput = document.querySelector('#total-expenses');
+const totalButton = document.querySelector('#btn-total');
+const contactList = document.querySelector('#contact-list');
+const totalContactOutput = document.querySelector('#total-contact');
 const alertInputControl = document.querySelector('ion-alert-controller');
 
-let myTotalExpenses = 0;
+let myTotalContacts = 0;
 
 const clear =() => {
-    reasonInput.value = '';
-    amountInput.value = '';
+    nameInput.value = '';
+    numberInput.value = '';
 };
 
 cancelButton.addEventListener('click', clear);
 
 addButton.addEventListener('click', () => {
-    console.log("Expense added!");
-    const reasonEntered = reasonInput.value;
-    const amountEntered = amountInput.value;
+    console.log("Contact added!");
+    const nameEntered = nameInput.value;
+    const numberEntered = numberInput.value;
     
-
-
-    if (reasonEntered.trim().length <= 0 || amountEntered <= 0 ||
-        amountEntered.trim().length <= 0){
-            alert("Please input reason and amount!")
+    if (nameEntered.trim().length <= 0 || numberEntered <= 0 ||
+        numberEntered.trim().length <= 0){
+            alert("Please input Contact name and Contact number!")
            
             return;
         }    
-        console.log(reasonEntered, amountEntered);
+        const newContact = document.createElement('ion-item');
+        newContact.textContent = nameEntered + ': ' + numberEntered;
+        contactList.appendChild(newContact);
 
-        const newExpenses = document.createElement('ion-item');
-        newExpenses.textContent = reasonEntered + ': P' + amountEntered;
-        expensesList.appendChild(newExpenses);
-
-       
-        myTotalExpenses += +amountEntered;
-        
-        totalExpensesOutput.textContent = myTotalExpenses;
+        myTotalContacts ++;
+        console.log(nameEntered, numberEntered);
+        totalContactOutput.textContent = myTotalContacts;
         clear();
 });
